62ad402f780f61317391b0608eae3d2c099b3e6a
Ahmed98-code <ahmed.darrag@engimtorino.net>
2021-03-02 10:21:32 +0100

    Prima della pausa

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,8 +12,12 @@ export class ProductListComponent implements OnInit {
   share() {
     window.alert('The product has been shared!');
   }
-  constructor() { }
-
+  constructor() { 
+    window.alert('The product has been shared!')
+  }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
   ngOnInit(): void {
   }
 
